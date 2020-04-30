0ddc139fe8de35f8c265c1bfed5d34ba08e94dc4
sk7w4tch3r <hdamghanian77@gmail.com>
2020-04-30 12:36:27 +0430

    for future development

---
+++ main/context_processor.py
@@ -0,0 +1,7 @@
+# from patient.models import info
+
+# def patients(request):
+#     doctor = request.user
+#     patients = info.objects.filter(doctor_id__exact=doctor)
+
+#     return {"patients": patients}
