d2213988591eaf0806670d96d73c8c272f181dc6
sk7w4tch3r <hdamghanian77@gmail.com>
2020-04-30 12:38:03 +0430

    adding new template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from patient.models import info
+
 
 def dashboard(request):
-    return render(request, 'dashboard/index.html')
+
+    return render(request,
+                  template_name='page.html',)
