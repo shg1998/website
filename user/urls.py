9fd01e2a60814f9c27be55501b8a6aecf3ca99c4
Hossein <hossenmsignin@gmail.com>
2020-05-22 16:48:30 +0430

    fixed profile view

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import ProfileDetailView, register, ProfileUpdateView, ProfileDeleteView
+from .views import ProfileDetailView, register, ProfileUpdateView, UserDeleteView, HospitalDetailView
 
 urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
 
+    path('hospital/<int:hospital_id>', HospitalDetailView.as_view(), name='hospital-detail'),
+
     path('profile/', ProfileDetailView.as_view(), name='profile-detail'),
     path('registration/', register, name='profile-create'),
-    path('profile/update', ProfileUpdateView.as_view(), name='profile-update'),
-    path('profile/delete/', ProfileDeleteView.as_view(), name='profile-delete'),
+    path('profile/update/', ProfileUpdateView.as_view(), name='profile-update'),
+    path('profile/delete/', UserDeleteView.as_view(), name='user-delete'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,9 @@
 from django.urls import reverse
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import DeleteView, DetailView, UpdateView
-from .models import UserProfile
+from .models import UserProfile, Hospital
 
 
 class ProfileDetailView(LoginRequiredMixin, DetailView):
@@ -30,28 +31,35 @@ def register(request):
 
 class ProfileUpdateView(LoginRequiredMixin, SuccessMessageMixin, UserPassesTestMixin, UpdateView):
     model = UserProfile
-    success_message = "patient updated successfully"
-    fields = ['name_pati', 'description_pati']
+    success_message = "profile updated successfully"
+    fields = ['name_prof', 'image_prof', 'hosp_prof']
 
-    def form_valid(self, form):
-        form.instance.doctor_pati = self.request.user
-        return super().form_valid(form)
+    def get_object(self, queryset=None):
+        return UserProfile.objects.filter(user_prof=self.request.user).get()
 
     def test_func(self):
-        patient = self.get_object()
-        if patient.doctor_pati == self.request.user:
+        profile = self.get_object()
+        if profile.user_prof == self.request.user:
             return True
         return False
 
 
-class ProfileDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = UserProfile
+class UserDeleteView(LoginRequiredMixin, DeleteView):
+    model = User
+    template_name = "user/user_confirm_delete.html"
+
+    def get_object(self, queryset=None):
+        return self.request.user
 
     def get_success_url(self):
-        return reverse('patient-create')
+        return reverse('base-home')
 
-    def test_func(self):
-        patient = self.get_object()
-        if patient.doctor_pati == self.request.user:
-            return True
-        return False
+
+# ********* ImagesViews bellow *********
+class HospitalDetailView(LoginRequiredMixin, DetailView):
+    model = Hospital
+
+    def get_object(self):
+        object=UserProfile.objects.filter(user_prof=self.request.user).get().hosp_prof.all()[self.kwargs["hospital_id"]]
+        self.pk=object.pk
+        return object
