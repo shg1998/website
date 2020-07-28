8173215a868a45ca7de273ee34e3dd67ef1862af
Sk7w4tch3r <32716448+Sk7w4tch3r@users.noreply.github.com>
2020-07-28 23:03:00 +0430

    Revert "some late commits"
    This reverts commit e02b41a5a93fbea8be34763468848bd6e78b711f.

--- base_app/webservice.py
+++ base_app/webservice.py
@@ -1,8 +1,6 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from patient.models import ImagePatient
-from django.shortcuts import redirect
-import json
 
 
 @login_required
@@ -11,8 +9,7 @@ def getImage(request, patient_id, image_id):
     if object.patient_imag.doctor_pati != request.user: return True
 
     image_url=object.image_imag.url
-    return redirect("http://127.0.0.1:8000"+image_url)
-    # return HttpResponse("http://127.0.0.1:8000"+image_url)
+    return HttpResponse("http://127.0.0.1:8000/"+image_url)
 
 
 @login_required
@@ -21,8 +18,7 @@ def getPoints(request, patient_id, image_id):
     if object.patient_imag.doctor_pati != request.user: return True
 
     points=object.points_imag
-    return JsonResponse(json.dumps(points), safe=False)
-    # return HttpResponse('<p>'+str(points)+'</p>')
+    return HttpResponse('<p>'+str(points)+'</p>')
 
 @login_required
 def setPoints(request, patient_id, image_id):
