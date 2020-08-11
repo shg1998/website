5c17a18cb8b43d783bda778635d0c84855e256d2
sk7w4tch3r <hdamghanian77@gmail.com>
2020-08-11 19:57:11 +0430

    merged with setPoints

--- base_app/tasks.py
+++ base_app/tasks.py
@@ -1,12 +1,17 @@
 from celery import shared_task
-import random
+from random import randrange
 import requests
+from patient.models import ImagePatient
+from .webservice import setPoints
 
 
 @shared_task
 def nnService(image_url, patient_id, image_idx):
     
-    setPoint_service_url = "127.0.0.1:8000/webservice/setPoints/"
-    setResults = requests.post()
-    
-    return None
+    # setPoint_service_url = "http://127.0.0.1:8000/webservice/setPoints/" + str(patient_id) + "/" + str(image_idx) + "/"
+    points = [ {'xpos': randrange(0, 500), 'ypos': randrange(0, 500)} for i in range(randrange(15))]
+    # setResult = requests.post(setPoint_service_url, data=load)
+    # print(setResult.status_code)
+    object = ImagePatient.objects.filter(patient_imag=patient_id)[image_idx]
+    object.points_imag = points
+    return points
