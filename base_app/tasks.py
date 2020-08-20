53637d5d5d4fea75c10cddab2d261e720ad976bc
sk7w4tch3r <hdamghanian77@gmail.com>
2020-08-20 18:17:15 +0430

    final touches (i hope:)

--- base_app/tasks.py
+++ base_app/tasks.py
@@ -1,53 +1,51 @@
-from celery import shared_task
-from random import randrange
-import requests
+import os
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from PIL import Image
+
 from patient.models import ImagePatient
 from .webservice import setPoints
 
+from celery import shared_task
+import requests
+from io import BytesIO
 
 @shared_task
 def nnService(image_url, patient_id, image_idx):
-    import torch
-    import torchvision
-    import cv2
-    import torchvision.transforms as transforms
-    from PIL import Image
-
+      
     transform = transforms.ToTensor()
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[1, 1, 1])
     res_to = 256
-    number_of_label_points = 9
-    model_path = ".\checkpoint_202008061041.pth.tar"
-    image_address = image_url
-    image_name = "test1"
-    img = cv2.imread(image_address)
-    img_x256 = cv2.resize(img, (res_to, res_to), cv2.INTER_CUBIC)
-    cv2.imwrite('out/' + image_name + "_" + str(res_to) + ".jpg", img_x256)
+    model_path9 = "base_app/checkpoint_202008061041.pth.tar"
+    model_path3 = "base_app/checkpoint_3points_202008091543.pth.tar"
+    image_address = "http://127.0.0.1:8000"+image_url
+    response = requests.get(image_address)
 
-    model = torchvision.models.resnet18(num_classes=18).cpu()
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu'))['model_state_dict'], strict='False')
-    model.eval()
+    model9  = torchvision.models.resnet18(num_classes=18).cpu()
+    model3  = torchvision.models.resnet18(num_classes=6).cpu()
+    model9.load_state_dict(torch.load(model_path9, map_location=torch.device('cpu'))['model_state_dict'], strict='False')
+    model9.eval()
+    model3.load_state_dict(torch.load(model_path3, map_location=torch.device('cpu'))['model_state_dict'], strict='False')
+    model3.eval()
 
-    with Image.open('out/' + image_name + "_" + str(res_to) + ".jpg") as img:
+    with Image.open(BytesIO(response.content)) as img:
         input_image = img.convert('RGB')
+    input_image_resized = input_image.resize((res_to, res_to))
+    input_image_resized = transform(input_image_resized)
+    input_image_resized = normalize(input_image_resized)
+    input_image_resized = input_image_resized.unsqueeze(0)
+    input_image_resized = input_image_resized[:, :, 14:241, 14:241]
 
-    input = transform(input_image)
-    input = normalize(input)
-    input = input.unsqueeze(0)
-    input = input[:, :, 14:241, 14:241]
+    pred9 = model9(input_image_resized).detach().numpy()
+    pred3 = model3(input_image_resized).detach().numpy()
 
-    pred = model(input).detach().numpy()
-    print(pred)
+    my_list_9 = [round(x.item()) for x in pred9[0, :]]
+    my_list_3 = [round(x.item()) for x in pred3[0, :]]
+    my_list   = my_list_9[0:12] + my_list_3
 
-    for i in range(number_of_label_points):
-        img_x256 = cv2.circle(img_x256, (int(pred[0, 2 * i + 0].item()) + 14,
-                                         int(pred[0, 2 * i + 1].item()) + 14), 4, (0, 1, 0), 2)
-        img_x256 = cv2.circle(img_x256, (int(pred[0, 2 * i + 0].item()) + 14,
-                                         int(pred[0, 2 * i + 1].item()) + 14), 4, (1, 0, 0), 2)
-        cv2.imwrite('out/labeled_' + image_name + "_" + str(res_to) + ".jpg", img_x256)
-
-    points = [[randrange(0, 500),randrange(0, 500)] for i in range(randrange(15))]
-
+    points = [[my_list[2*i],my_list[2*i+1]] for i in range(int(len(my_list)/2))]
+    print(points)
     object = ImagePatient.objects.filter(patient_imag=patient_id)[image_idx]
     object.points_imag = points
     object.save()
